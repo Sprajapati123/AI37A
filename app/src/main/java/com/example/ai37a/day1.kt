ac3d3fafbc7bd9c5bdf9528a32e7c7422d7ef58c
Bipol Khadka <S367234@students.cdu.edu.au>
2025-11-05 13:27:04 +0545

    initial commit

---
+++ app/src/main/java/com/example/ai37a/day1.kt
@@ -0,0 +1,19 @@
+package com.example.ai37a
+
+fun main() {
+    val result = calculate(5,5)
+    print(result)
+}
+
+fun calculate(a:Int,b:Int) : Int{
+    return a+b
+}
+
+fun login(username:String,password:String) : Unit{
+
+}
+
+
+//Int calculate(int a,int b){
+//
+//}
