5b233cf264368f28033f94a4d0030e21fdac1e9a
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-10 13:29:59 +0545

    repo

--- app/src/main/java/com/example/ai37a/model/UserModel.kt
+++ app/src/main/java/com/example/ai37a/model/UserModel.kt
@@ -5,7 +5,18 @@ data class UserModel(
     val firstName:String = "",
     val lastName:String = "",
     val email:String = "",
-    val password:String = "",
     val dob:String = "",
     val gender:String = "",
-)
+){
+
+    fun toMap() : Map<String,Any?>{
+        return mapOf(
+            "userId" to userId,
+            "firstName" to firstName,
+            "lastName" to lastName,
+            "email" to email,
+            "dob" to dob,
+            "gender" to gender,
+        )
+    }
+}
