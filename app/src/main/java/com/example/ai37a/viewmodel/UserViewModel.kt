62ea4885162926a50b0e812a78cb4eaa880b7da0
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-12 12:33:52 +0545

    viewmodel

--- app/src/main/java/com/example/ai37a/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/ai37a/viewmodel/UserViewModel.kt
@@ -7,73 +7,93 @@ import com.example.ai37a.model.UserModel
 import com.example.ai37a.repository.UserRepo
 import com.google.firebase.auth.FirebaseUser
 
-class UserViewModel(val repo: UserRepo) : ViewModel(){
+class UserViewModel(val repo: UserRepo) : ViewModel() {
 
     fun login(
         email: String, password: String,
         callback: (Boolean, String) -> Unit
-    ){
-       repo.login(email,password,callback)
+    ) {
+        repo.login(email, password, callback)
     }
 
     fun forgetPassword(
         email: String,
         callback: (Boolean, String) -> Unit
-    ){
-        repo.forgetPassword(email,callback)
+    ) {
+        repo.forgetPassword(email, callback)
     }
 
     fun updateProfile(
         userId: String, model: UserModel,
         callback: (Boolean, String) -> Unit
-    ){
-       repo.updateProfile(userId,model,callback)
+    ) {
+        repo.updateProfile(userId, model, callback)
     }
 
-    fun getCurrentUser(): FirebaseUser?{
+    fun getCurrentUser(): FirebaseUser? {
         return repo.getCurrentUser()
     }
 
-    fun deleteAccount(userId: String, callback: (Boolean, String) -> Unit){
-        repo.deleteAccount(userId,callback)
+    fun deleteAccount(userId: String, callback: (Boolean, String) -> Unit) {
+        repo.deleteAccount(userId, callback)
     }
 
-    fun logOut(callback: (Boolean, String) -> Unit){
+    fun logOut(callback: (Boolean, String) -> Unit) {
         repo.logOut(callback)
     }
 
     private val _users = MutableLiveData<UserModel?>()
-    val users : MutableLiveData<UserModel?>
+    val users: MutableLiveData<UserModel?>
         get() = _users
 
     private val _allUsers = MutableLiveData<List<UserModel>?>()
-    val allUsers : MutableLiveData<List<UserModel>?>
+    val allUsers: MutableLiveData<List<UserModel>?>
         get() = _allUsers
 
     private val _loading = MutableLiveData<Boolean>()
-    val loading : MutableLiveData<Boolean>
+    val loading: MutableLiveData<Boolean>
         get() = _loading
 
     fun getUserById(
         userId: String,
-    ){
-
+    ) {
+        _loading.postValue(true)
+        repo.getUserById(userId) { success, msg, data ->
+            if (success) {
+                _loading.postValue(false)
+                _users.postValue(data)
+            } else {
+                _loading.postValue(false)
+                _users.postValue(null)
+            }
+        }
     }
 
-    fun getAllUser(){
-
+    fun getAllUser() {
+        _loading.postValue(true)
+        repo.getAllUser { success, msg, data ->
+            if (success) {
+                _loading.postValue(false)
+                _allUsers.postValue(data)
+            } else {
+                _loading.postValue(false)
+                _allUsers.postValue(null)
+            }
+        }
     }
 
-    fun register(email: String, password: String,
-                 callback: (Boolean, String,String) -> Unit){
-        repo.register(email,password,callback)
+    fun register(
+        email: String, password: String,
+        callback: (Boolean, String, String) -> Unit
+    ) {
+        repo.register(email, password, callback)
     }
 
     fun addUserTODatabase(
         userId: String,
         model: UserModel,
         callback: (Boolean, String) -> Unit
-    ){
-      repo.addUserTODatabase(userId,model,callback)
+    ) {
+        repo.addUserTODatabase(userId, model, callback)
     }
 }
